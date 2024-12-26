75dadef5a9a6bf21aef8ba390d6c7f81a9db0646
azigani <azigani8@gmail.com>
2024-12-26 16:41:14 +0000

    initialisation du projet

---
+++ backendbank/src/main/java/com/alponse/backendbank/BackendbankApplication.java
@@ -0,0 +1,13 @@
+package com.alponse.backendbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbankApplication.class, args);
+	}
+
+}
---
+++ backendbank/src/test/java/com/alponse/backendbank/BackendbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alponse.backendbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
